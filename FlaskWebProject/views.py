f97ecb6f8eb194a947c4606405cc6c52e7f72306
Pragathi Kallu <p.kallu@outlook.com>
2022-05-12 15:45:07 -0500

    first commit

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -88,7 +88,7 @@ def authorized():
     if request.args.get('code'):
         cache = _load_cache()
         # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
-         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
+        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
                             request.args['code'],
                             scopes=Config.SCOPE,
                             redirect_uri=url_for('authorized', _external=True, _scheme='https'))
@@ -132,7 +132,7 @@ def _build_msal_app(cache=None, authority=None):
     client_credential=Config.CLIENT_SECRET, token_cache=cache)
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-   return _build_msal_app(authority=authority).get_authorization_request_url(
+    return _build_msal_app(authority=authority).get_authorization_request_url(
     scopes or [],
     state=state or str(uuid.uuid4()),
     redirect_uri=url_for('authorized', _external=True, _scheme='https'))
